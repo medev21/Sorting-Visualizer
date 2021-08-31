af90ee1d1a55ae4d6f980526a838813072a9a2f7
medev21 <benmarteen07@gmail.com>
2021-08-30 19:56:58 -0500

    test lint husky

--- src/algorithms/bubbleSort.js
+++ src/algorithms/bubbleSort.js
@@ -1,24 +1,14 @@
-export const bubbleSort = function *(arrStore) {
-
-    const arr = arrStore.arr;
-    // let temp;
-    const colorCompare = 'red';
-
-    for(let i = 0; i < arr.length; i++){
-    console.log("🚀 ~ file: bubbleSort.js ~ line 8 ~ arr.length", arr.length)
-
-        for(let j=1; j < arr.length; j++){
-
-            yield arrStore.compare(i,j, colorCompare)
-            if(arr[j-1] > arr[j]){
-                // temp = arr[j-1];
-                // arr[j-1] = arr[j];
-                // arr[j] = temp;
-                yield arrStore.swap(j-1,j);
-            }
-        }
-
+const bubbleSort = function* (arrStore) {
+  const { arr } = arrStore;
+  const colorCompare = 'red';
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+      yield arrStore.compare(i, j, colorCompare);
+      if (arr[j - 1] > arr[j]) {
+        yield arrStore.swap(j - 1, j);
+      }
     }
+  }
 };
 
-// export default bubbleSort;
+export default bubbleSort;
